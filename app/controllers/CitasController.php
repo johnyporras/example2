7a03d37fd3458aef1add758721380e00eaebfe3a
Vaioreto <javier.alberto.lugo@gmail.com>
2017-01-30 12:57:05 -0800

    a partir de la linea 63 de este archivo en este comit hay unas dudas, todas estan comentadas

--- app/controllers/CitasController.php
+++ app/controllers/CitasController.php
@@ -53,9 +53,44 @@ class CitasController extends \Phalcon\Mvc\Controller
 
         $objDatos =  json_decode( $request->getPost('obj') );
 
+        $clave = new AcClaves();
+        $clave->clave = 'ODHEYCL';
+        $clave->cedula_afiliado = $objDatos->cedula_afiliado;
+        $clave->codigo_contrato = $objDatos->codContra;
+        $clave->fecha_cita = $objDatos->fecha_cita;
+        $clave->motivo = $objDatos->motivo;
+        $clave->observaciones = $objDatos->observaciones;
+        $clave->costo_total = $objDatos->montoTotal; //preguntar si esta bien subir de una vez el monto total
+        $clave->codigo_proveedor_creador = $objDatos->proveedor->codigo_proveedor; //preguntar si basta simplemente con el codigo del proveedor
+        $clave->correo = $objDatos->email;
+        $clave->examen = '??'; //preguntar los examenes van en detail claves como colocare examen aqui en caso de que sean varios?
+        $clave->estatus_clave = 1; //preguntar si 1 para activarlo de una vez
+        $clave->creador = 1; //preguntar que fucking es esto y si con mandar 1 basta
+        $clave->telefono = $objDatos->telefono;
+        $clave->rechazo = '???'; //preguntar si mandar vacio o que?
+        $clave->tipo_afiliado = $objDatos->tipoContra == 'B' ? 0 : 1; //preguntar tipo de contrato es igual a tipo de afiliado?
+        $clave->cantidad_servicios = $objDatos->cantServ;
+        $clave->save();
+
+        //aqui se mandan los detalles de los servicios que tienen que ver con la espacialidad, de igual forma estos valores los puedes chekr una vez se guarden
+
+        foreach ($objDatos->detailClaveServ as $item ) {
+            
+            $claveDetalle = new AcClavesDetalle();
+            $claveDetalle->id_clave = $clave->id;
+            $claveDetalle->codigo_servicio = $item->tipoServ->codigo_servicio;
+            $claveDetalle->codigo_especialidad = $objDatos->espec;
+            $claveDetalle->id_procedimiento = $item->proMed->id;
+            $claveDetalle->codigo_proveedor = $objDatos->proveedor->codigo_proveedor;
+            $claveDetalle->costo = $item->monto;
+            $claveDetalle->detalle = $objDatos->detailServ;
+            $claveDetalle->save();
+
+        }
+
         $status = 200;
         $msnStatus = 'OK';
-        $this->_data = $objDatos;
+        $this->_data = $clave->clave;
         $this->_mensajes = [
             "msnConsult" => 'Consulta relizada con exito',
         ];
